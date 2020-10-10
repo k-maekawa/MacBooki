43904d4ed3a2ec564e0bfb5c79697e8c3b62e764
hasihara <71763800+hasihara@users.noreply.github.com>
2020-10-10 16:52:29 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -13,11 +13,11 @@ Rails.application.routes.draw do
   post "users/create" => "users#create"
   get "singnup" => "users#new"
   get "users/index" => "users#index"
-  get "users/:id" => "users#show"
+  get "users/:id" => "users#show"as: :user
   
   get "posts/index" => "posts#index"
   get "posts/new" => "posts#new"
-  get "posts/:id" => "posts#show"
+  get "posts/:id" => "posts#show", as: :post
    
   resources :comments, only: [:create]
   
