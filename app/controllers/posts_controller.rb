26b73c05196e744b6d6770b6e00924cc7dc5dd77
hasihara <71763800+hasihara@users.noreply.github.com>
2020-10-10 16:50:45 +0900

    Update posts_controller.rb

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,7 +8,7 @@ class PostsController < ApplicationController
   
   def show
     @post = Post.find_by(id: params[:id])
-    @user = @post.user
+    @user = @post
     @post = Post.find(params[:id])
     @comments = @post.comments
     @comment = @comments.new
