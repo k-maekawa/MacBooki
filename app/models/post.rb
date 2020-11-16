6659e025b06d1c9ed0175855bcc9a74b2c463279
hasihara <71763800+hasihara@users.noreply.github.com>
2020-11-16 20:41:24 +0900

    Update post.rb

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,7 @@ class Post < ApplicationRecord
   validates :content, {presence: true, length: {maximum: 140}}
   validates :user_id, {presence: true}
   has_many :comments
-  
+  mount_uploader :video, VideoUploader
   def user
     return User.find_by(id: self.user_id)
   end
